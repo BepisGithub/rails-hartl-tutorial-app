7ca9cc89d846ea042200d0e5a3f8e87ebea33d70
bepis <luqdek@gmail.com>
2021-05-02 07:15:59 +0100

    make a basic user model (including secure passwords)

---
+++ TEMP.rb
@@ -0,0 +1,17 @@
+Asalamualykum (means peace be upon you), it's good to hear you have accepted islam. 
+Congratulations on being blessed with guidance, and I ask Allah to only increase you in guidance.
+
+I want to let you know that now you have become a muslim, you are just as much of a muslim as anyone else. 
+No one is allowed to tell you that you are less of a muslim based on your skin colour or on the amount of time you have been a muslim. 
+
+"O people, your Lord is one and your father Adam is one. T
+here is no favor of an Arab over a foreigner, nor a foreigner over an Arab, 
+and neither white skin over black skin, nor black skin over white skin, except by righteousness.
+ Have I not delivered the message?" - The prophet Muhammad (peace be upon him).
+
+No doubt there's a lot to learn, but that's true for all muslims all the time. You should learn firstly about the five pillars. They are: belief/testimony, prayer, zakaat (a type of charity), fasting, hajj (pilgrimage to the building called the kaabah in makkah, saudi arabia).
+
+Below I will attach some resources I recommend so you can learn more. Feel free also to contact me on whatsapp.
+https://quran.com/
+
+https://islamqa.info/en - a website that releases many islamic rulings on various topics. there's a lot of good information here. if you ever have a question and don't know who to ask / can't find anyone to ask then this should suffice.
