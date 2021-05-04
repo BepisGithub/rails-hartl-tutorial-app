5800215e9800265c62230c102ae41dc2a4374ab3
bepis <luqdek@gmail.com>
2021-05-04 05:09:06 +0100

    implement basic login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,26 @@ class SessionsController < ApplicationController
   end
 
   def create
-
+    user = User.find_by_email(params[:session][:email].downcase)
+    # stop if @user.nil?
+    if user && user.authenticate(params[:session][:password])
+      # log in
+      log_in(user)
+      redirect_to user
+    else
+      # dont log in and instead show errors + log in form
+      flash.now[:danger] = 'Wrong log in information!'
+      render 'new'
+    end
   end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+
+  private
+    def session_params
+      params.permit(:session).require(:email, :password)
+    end
 end
---
+++ test/integration/users_login_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+class UsersLoginTest < ActionDispatch::IntegrationTest
+  test 'login with invalid information' do
+    get login_path
+    post login_path, params: {session: {email: 'invalid@invalid,org', password: 'foo'}}
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+  end
+end
